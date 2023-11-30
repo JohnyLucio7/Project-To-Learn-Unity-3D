e2fb27c244f4abb7a6bbd8e1145cb44e20b9be43
Johny Lúcio <59095834+JohnyLucio7@users.noreply.github.com>
2023-11-30 09:40:42 -0300

    7 - MECÂNICA PRIMEIRA PESSOA - CONTROLE DO PERSONAGEM - SIMPLES

--- Assets/Scripts/PlayerFPS.cs
+++ Assets/Scripts/PlayerFPS.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerFPS : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private CharacterController characterController;
+    private Vector3 velocity;
+
+    public float speed;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.right * x + transform.forward * z) * speed * Time.deltaTime;
+
+        characterController.Move(move);
     }
 }
