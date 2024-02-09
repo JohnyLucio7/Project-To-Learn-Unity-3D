1f5534a663a73a7ce385aafd6d71dc202335da42
Johny Lúcio <59095834+JohnyLucio7@users.noreply.github.com>
2024-02-09 09:37:39 -0300

    15 - APRIMORANDO O ÍCONE DE EXIBIÇÃO

--- Assets/Scripts/Ico_Interaction.cs
+++ Assets/Scripts/Ico_Interaction.cs
@@ -7,35 +7,74 @@ public class Ico_Interaction : MonoBehaviour
 {
     GameController gameController;
     public Image ico;
+    public Text txtDistance;
     public GameObject canvas;
 
     private bool isBehind;
+    public bool isShowDistance;
 
     private void Awake()
     {
         gameController = FindObjectOfType<GameController>();
     }
 
+    private void Start()
+    {
+        txtDistance.text = "";
+        txtDistance.gameObject.SetActive(false);
+        canvas.SetActive(false);
+    }
+
     void Update()
     {
-        if (Vector3.Distance(transform.position, gameController.PlayerBody.position) <= gameController.icoInteractionDistance)
+
+        if (isShowDistance == false)
         {
-            canvas.SetActive(true);
-
-            Vector3 toOther = transform.position - gameController.PlayerBody.position;
-            Vector3 forward = gameController.PlayerBody.forward;
-
-            isBehind = Vector3.Dot(toOther, forward) < 0;
-
-            if (isBehind == false)
+            if (Vector3.Distance(transform.position, gameController.PlayerBody.position) <= gameController.icoInteractionDistance)
             {
-                Vector2 icoPos = Camera.main.WorldToScreenPoint(transform.position);
-                ico.transform.position = icoPos;
+                canvas.SetActive(true);
+
+                UpdateCanvas();
+            }
+            else
+            {
+                canvas.SetActive(false);
             }
         }
         else
         {
-            canvas.SetActive(false);
+            canvas.SetActive(true);
+            UpdateCanvas();
+        }
+    }
+
+    void UpdateCanvas()
+    {
+        Vector3 toOther = transform.position - gameController.PlayerBody.position;
+        Vector3 forward = gameController.PlayerBody.forward;
+
+        isBehind = Vector3.Dot(toOther, forward) < 0;
+
+        float distance = Vector3.Distance(transform.position, gameController.PlayerBody.position);
+
+        if (isShowDistance)
+        {
+            if (distance > 2)
+            {
+                txtDistance.gameObject.SetActive(true);
+            }
+            else
+            {
+                txtDistance.gameObject.SetActive(false);
+            }
+        }
+
+        txtDistance.text = distance.ToString("N0") + "m";
+
+        if (isBehind == false)
+        {
+            Vector2 icoPos = Camera.main.WorldToScreenPoint(transform.position);
+            ico.transform.position = icoPos;
         }
     }
 }
